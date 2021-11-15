d87699e0b504d9c1faa4d30c5d005f77a89d911e
JimHofm52 <JimHofmann52@gmail.com>
2021-11-15 14:24:33 -0500

    Added comments & sdbInit for scaleFactor.

--- RO-2021/src/main/java/frc/robot/subsystem/Drive.java
+++ RO-2021/src/main/java/frc/robot/subsystem/Drive.java
@@ -12,20 +12,44 @@ public class Drive {
 
     private static double scaleFactor;
 
+    /**
+     * Drive the robot using tank drive.
+     * <p>
+     * <p>Constructor, not needed, just because.
+     */
     public Drive() {
     }
 
+    /**
+     * Initialize any objects.
+     * <p>Usually called from autonomous/telopPeriodic.
+     */
     public static void init() {
         scaleFactor = 1;
+        sdbInit();
     }
 
+    /**
+     * This is called from robot autonomous/telopPeriodic every 20mS to evaluate
+     * hardware or joystick actions to set the state.
+     * <p>
+     * <p>Left JS Y axis drives left drive wheel.  Right drives right. 
+     */
     public static void update() {
         leftDrive.set(JS_IO.axLeftDrive.get() * scaleFactor);
         rightDrive.set(-JS_IO.axRightDrive.get() * scaleFactor);
         sdbUpdate();
     }
 
+    /** Initialize any sdb objects that are later retrieve from sdb. */
+    private static void sdbInit(){
+        SmartDashboard.putNumber("Drive/Scale", scaleFactor);
+    }
+
+    /** Update sdb objects. */
     public static void sdbUpdate(){
-        SmartDashboard.putNumber("Right Drive", JS_IO.axRightDrive.get());
+        SmartDashboard.getNumber("Drive/Scale", scaleFactor);
+        SmartDashboard.putNumber("Drive/Left JS", JS_IO.axLeftDrive.get());
+        SmartDashboard.putNumber("Drive/Right JS", JS_IO.axRightDrive.get());
     }
 }
